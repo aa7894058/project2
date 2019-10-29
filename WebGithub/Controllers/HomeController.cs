b585248c598e78d10f796042567c319ba0c58ce0
aa7894058 <aa7894058@gmail.com>
2019-10-29 15:55:49 +0800

    登入登出

--- WebGithub/Controllers/HomeController.cs
+++ WebGithub/Controllers/HomeController.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WebGithub.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["project7ConnectionString"].ConnectionString);
         // GET: Default
         public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string id, string pwd)
+        {
+            string sql = "select * from MemberAccount where mbrAccount = @mbrAccount and mbrPwd=@mbrPwd";
+            SqlCommand cmd = new SqlCommand(sql, Conn);
+            cmd.Parameters.AddWithValue("@mbrAccount", id);
+            cmd.Parameters.AddWithValue("@mbrPwd", pwd);
+            SqlDataReader rd;
+
+            Conn.Open();
+            rd = cmd.ExecuteReader();
+            if (rd.Read())
+            {
+                Session["id"] = rd["mbrPwd"].ToString();
+
+
+                Conn.Close();
+                return RedirectToAction("Indexmain", "Home");
+            }
+            Conn.Close();
+            ViewBag.LoginErr = "帳號或密碼有誤";
+            return View();
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Indexmain()
         {
             return View();
         }
---
+++ WebGithub/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebGithub.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:project7ConnectionString %>" SelectCommand="SELECT * FROM [actBulletin]"></asp:SqlDataSource>
+
+        </div>
+    </form>
+</body>
+</html>
